5f4f4821d66c8d65a2004c1ec221f41725de4942
Etty Werczberger <ewerczbe@student.touro.edu>
2025-12-31 17:28:24 +0200

    RS1F2-265: implemented RecipeCreatedEvent.java

---
+++ recipeshare/src/main/java/com/mcon152/recipeshare/event/RecipeCreatedEvent.java
@@ -0,0 +1,18 @@
+public class RecipeCreatedEvent {
+
+    private final Long recipeId;
+    private final Long authorId;
+
+    public RecipeCreatedEvent(Long recipeId, Long authorId) {
+        this.recipeId = recipeId;
+        this.authorId = authorId;
+    }
+
+    public Long getRecipeId() {
+        return recipeId;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+}
